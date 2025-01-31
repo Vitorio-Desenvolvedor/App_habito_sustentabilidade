48bfd3efc579f31347d9a5e16280fa5a25222f2e
Emerson <eb3266907@gmail.com>
2025-01-30 22:40:14 -0300

    Adicionei a funcionalidade de deletar tarefas

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ let input_tarefa = document.getElementById("input-tarefa");
 let btn_addTarefa = document.getElementById("btn-addtarefa");
 let containerTarefas = document.getElementById("containerTarefas");
 const dicionarioTarefas = {};
+let contador_tarefas = 0;
 
 
 function funcAddTarefa(){
     let texto_tarefa = input_tarefa.value;
+    let id_tarefa = `${contador_tarefas}${texto_tarefa}`;
+    let strId_tarefa = `'${id_tarefa}'`;
+    dicionarioTarefas[strId_tarefa] = texto_tarefa;
+
     if ((texto_tarefa === "") || (texto_tarefa === undefined) || (texto_tarefa === null)){
         input_tarefa.placeholder = "Digite uma tarefa para poder adicioná-la";
         return;
     };
     containerTarefas.innerHTML += 
-    `<div class="tarefa">
+    `<div id="${id_tarefa}" class="tarefa">
         <div class="tarefa-icon">
             <span class="material-icons">radio_button_unchecked</span>
         </div>
@@ -22,17 +27,26 @@ function funcAddTarefa(){
             <div class="btn-editar">
                 <span class="material-icons">edit</span>
             </div>
-            <div class="btn-delete">
+            <div onclick="deleteTarefa(${strId_tarefa})" class="btn-delete">
                 <span class="material-icons">delete_outline</span>
             </div>
         </div>
     </div>`;
     input_tarefa.value = "";
     input_tarefa.onfocus();
+    contador_tarefas += 1;
+    // console.log(dicionarioTarefas);
 
 }
 function limparPlaceholder(){
     input_tarefa.placeholder = "Digite sua tarefa";
+}
+function deleteTarefa(id_tarefa){
+    // containerTarefas.remove();
+    delete dicionarioTarefas[`'${id_tarefa}'`];
+    let tarefa = document.getElementById(id_tarefa);
+    tarefa.remove();
+    // console.log(dicionarioTarefas)
 }
 
 input_tarefa.addEventListener("keyup", function(event) {
