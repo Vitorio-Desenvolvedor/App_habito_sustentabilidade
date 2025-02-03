98efb57e698e00c58ba3d40057db2698b3846ca6
Emerson <eb3266907@gmail.com>
2025-02-03 17:27:49 -0300

    Adicionando as funcionalidades as tarefas concluidas

--- index.js
+++ index.js
@@ -23,15 +23,30 @@ function funcAddTarefa(){
 
 };
 function deletarTarefa(id_tarefa){
-    // console.log(id_tarefa)
-    delete dicionarioAFazer[`'${id_tarefa}'`];
-    btnAFazer();
+    let real_idTarefa = `'${id_tarefa}'`;
+    let tarefa_html = document.getElementById(real_idTarefa);
+    let temClasseMarcada = tarefa_html.classList.contains('marcada');
+    if (temClasseMarcada){
+        delete dicionarioConcluidas[real_idTarefa];
+        btnConcluidas();
+    }else {
+        delete dicionarioAFazer[real_idTarefa];
+        btnAFazer();
+    }
 };
 function marcarTarefa(id_tarefa){
-    real_idTarefa = `'${id_tarefa}'`;
-    dicionarioConcluidas[real_idTarefa] = dicionarioAFazer[real_idTarefa];
-    delete dicionarioAFazer[real_idTarefa];
-    btnAFazer();
+    let real_idTarefa = `'${id_tarefa}'`;
+    let tarefa_html = document.getElementById(real_idTarefa);
+    let temClasseMarcada = tarefa_html.classList.contains('marcada');
+    if (temClasseMarcada){
+        dicionarioAFazer[real_idTarefa] = dicionarioConcluidas[real_idTarefa];
+        delete dicionarioConcluidas[real_idTarefa];
+        btnConcluidas();
+    }else {
+        dicionarioConcluidas[real_idTarefa] = dicionarioAFazer[real_idTarefa];
+        delete dicionarioAFazer[real_idTarefa];
+        btnAFazer();
+    }
 };
 
 function btnAFazer(){
@@ -39,7 +54,7 @@ function btnAFazer(){
     for (const id_tarefa in dicionarioAFazer){
         if (dicionarioAFazer.hasOwnProperty(id_tarefa)){
             tarefa =     
-            `<div class="tarefa">
+            `<div id="${id_tarefa}" class="tarefa">
                 <div onclick="marcarTarefa(${id_tarefa})" class="tarefa-icon">
                     <span class="material-icons">radio_button_unchecked</span>
                 </div>
@@ -61,10 +76,34 @@ function btnAFazer(){
     }
     btn_aFazer.innerHTML = `A Fazer (${Object.keys(dicionarioAFazer).length})`;
     btn_concluidas.innerHTML = `Concluídas (${Object.keys(dicionarioConcluidas).length})`;
-
-
-
-
+};
+function btnConcluidas(){
+    containerTarefas.innerHTML = "";
+    for (const id_tarefa in dicionarioConcluidas){
+        if (dicionarioConcluidas.hasOwnProperty(id_tarefa)){
+            tarefa =     
+            `<div id="${id_tarefa}" class="tarefa marcada">
+                <div onclick="marcarTarefa(${id_tarefa})" class="tarefa-icon">
+                    <span class="material-icons">check_circle_outline</span>
+                </div>
+                <div onclick="marcarTarefa(${id_tarefa})" class="tarefa-texto">
+                    ${dicionarioConcluidas[id_tarefa]}
+                </div>
+                <div class="tarefa-opcoes">
+                    <div class="btn-editar">
+                        <span class="material-icons">edit</span>
+                    </div>
+                    <div onclick="deletarTarefa(${id_tarefa})" class="btn-delete">
+                        <span class="material-icons">delete_outline</span>
+                    </div>
+                </div>
+            </div>`;
+    
+            containerTarefas.innerHTML += tarefa;
+        }
+    }
+    btn_aFazer.innerHTML = `A Fazer (${Object.keys(dicionarioAFazer).length})`;
+    btn_concluidas.innerHTML = `Concluídas (${Object.keys(dicionarioConcluidas).length})`;
 };
 function limparPlaceholder(){
     input_tarefa.placeholder = "Digite sua tarefa";
