2a4d970c2bb6f8d3189df6bc5746b04630cf03cd
EMERSON BRUNO CARVALHO DA SILVA <a95517370@rn.senac.local>
2025-02-26 10:40:12 -0300

    Adicionei o design das eco tasks e adicionei o ranking

--- scripts/ecoTask.js
+++ scripts/ecoTask.js
@@ -1,6 +1,9 @@
 
 
-
+function toggleRanking(){
+    let containerRanking = document.getElementById("containerRanking");
+    containerRanking.classList.toggle("activeRanking");
+}
 
 function btnLinkOption(link){
     window.location.href = link;
