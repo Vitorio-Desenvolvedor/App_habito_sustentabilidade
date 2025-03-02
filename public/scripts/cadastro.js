20bcb90f9bd174ff7d6b7808798c3138405350dc
Emerson <eb3266907@gmail.com>
2025-03-01 22:28:54 -0300

    Adicionei um json para dados, fiz as funcionalides cadastro e login

---
+++ public/scripts/cadastro.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("formCadastro");
+const mensagemDiv = document.getElementById("mensagemVerificacao");
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const dados = Object.fromEntries(formData.entries());
+    // console.log(dados)
+
+    try {
+        const response = await fetch('/salvar-cadastro', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(dados)
+        });
+
+        const resultado = await response.json();
+
+        if (resultado.success){
+            if (mensagemDiv.classList.contains("fail")){
+                mensagemDiv.classList.remove("fail");
+            }
+            mensagemDiv.classList.add("sucess");
+            mensagemDiv.textContent = "Cadastro realizado!";
+            setTimeout(() => {
+                window.location.href = '/index.html';
+            }, 2000)
+        }else{
+            if (mensagemDiv.classList.contains("sucess")){
+                mensagemDiv.classList.remove("sucess");
+            }
+            mensagemDiv.classList.add("fail");
+            mensagemDiv.textContent = resultado.message;
+        }
+    } catch (error){
+        mensagemDiv.classList.add("fail")
+        mensagemDiv.textContent = "Erro na conexão com o servidor";
+    }
+});
